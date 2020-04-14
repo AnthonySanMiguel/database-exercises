c52819605e68bd91f032fe6fe6b43473d6044695
Anthony San Miguel <asanmiguel1088@gmail.com>
2020-04-14 09:34:04 -0500

    updated sql albums exercise following instructor walkthrough

--- albums_migration.sql
+++ albums_migration.sql
@@ -2,12 +2,12 @@ USE codeup_test_db;
 
 DROP TABLE IF EXISTS albums;
 CREATE TABLE IF NOT EXISTS albums (
-    id INT NOT NULL AUTO_INCREMENT,
-    artist VARCHAR (50) DEFAULT 'NONE',
-    name VARCHAR (50) NOT NULL,
-    release_date DATE NOT NULL,
-    sales INT,
-    genre VARCHAR (50),
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR (124) DEFAULT 'NONE',
+    name VARCHAR (124) NOT NULL,
+    release_date INT(4) UNSIGNED NOT NULL,
+    sales FLOAT NOT NULL,
+    genre VARCHAR (500) DEFAULT 'NONE',
     PRIMARY KEY (id)
     );
 SHOW TABLES;
